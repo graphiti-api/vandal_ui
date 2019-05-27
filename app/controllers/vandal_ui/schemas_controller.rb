696f4c4030cef188d695e12e30f20d870d0206b6
Lee Richmond <lrichmond1@bloomberg.net>
2019-05-27 11:41:51 -0400

    Ensure resources get loaded

--- app/controllers/vandal_ui/schemas_controller.rb
+++ app/controllers/vandal_ui/schemas_controller.rb
@@ -1,6 +1,9 @@
 class VandalUi::SchemasController < ActionController::API
   def show
     Rails.application.eager_load!
+    Dir.glob("#{Rails.root}/app/resources/*").each do |f|
+      require f
+    end
     render json: Graphiti::Schema.generate
   end
 end
