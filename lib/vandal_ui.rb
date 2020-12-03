222971a83344b1813bfbe950b89d06a352cd599c
Xheladin Popaj <xheladin.popaj@metoda.com>
2020-12-03 17:00:22 +0100

    update

--- lib/vandal_ui.rb
+++ lib/vandal_ui.rb
@@ -1,6 +1,6 @@
-require "vandal_ui/version"
-require "vandal_ui/railtie" if defined?(Rails)
-require "vandal_ui/engine" if defined?(Rails)
+require 'vandal_ui/version'
+require 'vandal_ui/railtie' if defined?(Rails)
+require 'vandal_ui/engine' if defined?(Rails)
 
 module VandalUi
 end
