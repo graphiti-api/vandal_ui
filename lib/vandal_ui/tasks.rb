f15fffe28344300c919891545de0dd28f5756f7e
XHPJ <xheladin.popaj@metoda.com>
2020-12-30 16:59:37 +0100

    update

--- lib/vandal_ui/tasks.rb
+++ lib/vandal_ui/tasks.rb
@@ -35,7 +35,7 @@ namespace :vandal do
 
     lines = IO.readlines(path).map do |line|
       if line.include?('__REMOTE_HOSTS__')
-        line.gsub('__REMOTE_HOSTS__', `"#{ENV.fetch('REMOTE_HOSTS')}"`, `"#{hosts}"`)
+        line.gsub('__REMOTE_HOSTS__', `"#{ENV.fetch('REMOTE_HOSTS')}"`, hosts.to_s)
       else
         line
       end
