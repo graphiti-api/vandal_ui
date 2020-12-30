be644e0f69c318032c6de12de4baf1162be8ee63
XHPJ <xheladin.popaj@metoda.com>
2020-12-30 16:22:48 +0100

    update

--- lib/vandal_ui/tasks.rb
+++ lib/vandal_ui/tasks.rb
@@ -23,7 +23,7 @@ namespace :vandal do
     path = "#{destination}/vandal/index.html"
     lines = IO.readlines(path).map do |line|
       if line.include?('__SCHEMA_PATH__') || line.include?('__HOSTS__')
-        line.gsub('__HOSTS__', ENV['REMOTE_HOSTS'])
+        line.gsub('__HOSTS__', ENV.fetch('REMOTE_HOSTS'))
         line.gsub('__SCHEMA_PATH__', ENV.fetch('SCHEMA_PATH', schema_path))
       else
         line
